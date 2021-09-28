c52d48521f9476af535da552421d7210fbcd5ab4
Syed Jamil <syed.jamil13@gmail.com>
2021-09-28 17:34:19 -0400

    Updated hmtl accordingly to semantics and defined required js variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+ var minLength = 8;
+ var maxLength = 128;
+
+ const Lower = 'q,w,e,r,t,y,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,b,n,m'.split("");
+ const Upper = lower.toUpperCase();
+ const Nmber = '0123456789'.split("");
+ const Symbol = "!\#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split("");
+
+ var confirmLength = "";
+ var confirmLower;
+ var confirmUpper;
+ var confirmSymbol;
+ var confirmNumber;
+ 
 
 // Write password to the #password input
 function writePassword() {
